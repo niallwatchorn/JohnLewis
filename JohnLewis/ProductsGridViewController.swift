b9e72f7968f21e0a201176a4db85a7b0f8abe2f2
Niall Watchorn <niallwatchorn@gmail.com>
2017-07-14 14:07:00 +0100

    Initial commit

---
+++ JohnLewis/ProductsGridViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsGridViewController.swift
+//  JohnLewis
+//
+//  Created by Niall Watchorn on 14/07/2017.
+//  Copyright © 2017 EpiTech. All rights reserved.
+//
+
+import UIKit
+
+class ProductsGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
---
+++ JohnLewisTests/ProductsGridViewControllerTests.swift
@@ -0,0 +1,18 @@
+//
+//  ProductsGridViewControllerTests.swift
+//  JohnLewis
+//
+//  Created by Niall Watchorn on 14/07/2017.
+//  Copyright © 2017 EpiTech. All rights reserved.
+//
+
+import XCTest
+
+class ProductsGridViewControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+}
