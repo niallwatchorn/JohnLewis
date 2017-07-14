55d05ff3be5222c05a040ade3f9254ecf5895309
Niall Watchorn <niallwatchorn@gmail.com>
2017-07-14 15:06:31 +0100

    Added ViewModel and unit tests for ViewModel

---
+++ JohnLewis/ProductsGridViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductsGridViewModel.swift
+//  JohnLewis
+//
+//  Created by Niall Watchorn on 14/07/2017.
+//  Copyright © 2017 EpiTech. All rights reserved.
+//
+
+import Foundation
+
+struct ProductsGridViewModel {
+    
+}
---
+++ JohnLewisTests/ProductsGridViewModelTests.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsGridViewModelTests.swift
+//  JohnLewis
+//
+//  Created by Niall Watchorn on 14/07/2017.
+//  Copyright © 2017 EpiTech. All rights reserved.
+//
+
+import XCTest
+
+class ProductsGridViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    
+}
