26a379e94079c7abed7d288ee6d185856be5eef2
Niall Watchorn <niallwatchorn@gmail.com>
2017-07-14 14:53:05 +0100

    Parse JSON from network layer

--- JohnLewis/ProductsNetworkAdapter.swift
+++ JohnLewis/ProductsNetworkAdapter.swift
@@ -16,6 +16,30 @@ class ProductsNetworkAdapter {
     }
     
     func getListOfProducts(_ completion: @escaping ([Dishwasher]?, Error?) -> Void) {
+        guard let url = Strings.url else {
+            completion(nil, nil)
+            return
+        }
         
+        let request = URLRequest(url: url)
+        networkManager.makeRequest(request: request) { (json, error) in
+            if error != nil {
+                completion(nil, error)
+            } else {
+                if let products = json?["products"] as? Array<[String: Any]> {
+                    var productsArray = [Dishwasher]()
+                    for productJSON in products {
+                        productsArray.append(Dishwasher(json: productJSON))
+                    }
+                    completion(productsArray, nil)
+                } else {
+                    completion(nil, NetworkError.productsNotFound)
+                }
+            }
+        }
     }
+}
+
+fileprivate struct Strings {
+    static let url = URL(string: "https://api.johnlewis.com/v1/products/search?q=dishwasher&key=Wu1Xqn3vNrd1p7hqkvB6hEu0G9OrsYGb&pageSize=20")
 }
